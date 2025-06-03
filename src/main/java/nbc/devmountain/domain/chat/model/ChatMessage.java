72f1f4b459a77fd8ea6d8cbc7100d53225281503
류형철 <fbgudcjf2@gmail.com>
2025-06-03 15:24:46 +0900

    feat: 채팅방 단건조회기능 위해서 연관관계 양방향으로 설정

--- src/main/java/nbc/devmountain/domain/chat/model/ChatMessage.java
+++ src/main/java/nbc/devmountain/domain/chat/model/ChatMessage.java
@@ -3,6 +3,7 @@ package nbc.devmountain.domain.chat.model;
 import jakarta.persistence.*;
 import lombok.*;
 import nbc.devmountain.domain.user.model.User;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,32 +11,38 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "ChatMessages")
+@Table(name = "ChatMessage")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class ChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long chatId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chatroom_id")
-    private ChatRoom chatRoom;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long chatId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chatroom_id")
+	private ChatRoom chatRoom;
 
-    private String message;
-    private Boolean isAiResponse;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userId")
+	private User user;
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+	private String message;
 
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+	private Boolean isAiResponse;
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	public void setChatRoom(ChatRoom chatRoom) {
+		this.chatRoom = chatRoom;
+	}
 }
