ff39d2701467fbead15b548094974f8df9cc50df
hugecookie <phrpp5770@gmail.com>
2025-06-04 09:09:37 +0900

    feat: JpaAuditing 메인 실행에 추가

--- src/main/java/nbc/devmountain/MountainApplication.java
+++ src/main/java/nbc/devmountain/MountainApplication.java
@@ -2,8 +2,10 @@ package nbc.devmountain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MountainApplication {
 
     public static void main(String[] args) {
