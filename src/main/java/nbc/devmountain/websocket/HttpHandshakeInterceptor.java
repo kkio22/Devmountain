85b152340bddaad2c27d032b1f13a3e88c08ef7d
류형철 <fbgudcjf2@gmail.com>
2025-06-05 19:40:25 +0900

    feat: 로그인유저 정보 가져오기 위한 interceptor 추가

--- src/main/java/nbc/devmountain/websocket/HttpHandshakeInterceptor.java
+++ src/main/java/nbc/devmountain/websocket/HttpHandshakeInterceptor.java
@@ -8,29 +8,58 @@ import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import nbc.devmountain.domain.user.model.User;
 
 public class HttpHandshakeInterceptor implements HandshakeInterceptor {
-	//사용자 인증검증을 위한 인터셉터
+
 	@Override
-	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+	public boolean beforeHandshake(ServerHttpRequest request,
+		ServerHttpResponse response,
+		WebSocketHandler wsHandler,
 		Map<String, Object> attributes) throws Exception {
+
 		HttpSession session = ((ServletServerHttpRequest)request).getServletRequest().getSession(false);
 
-		if(session !=null){
-			User user = (User)session.getAttribute("");//세션-스프링컨텍스트명
-			attributes.put("user",user); //
+		if (session != null) { //로그인 후 세션이 있음
+			User user = (User)session.getAttribute("LOGIN_USER");
+
+			if (user != null) {
+				attributes.put("user", user);
+				attributes.put("userId", user.getUserId());
+				String userType = getUserType(user.getMembershipLevel());
+				attributes.put("userType", userType);
+			} else {
+				// 비로그인 유저
+				setGuestAttributes(attributes);
+			}
+		} else {
+			//세션이 없는 비로그인 유저인경우
+			setGuestAttributes(attributes);
 		}
 
 		return true;
-
 	}
 
 	@Override
 	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
 		Exception exception) {
+	}
 
+	private String getUserType(User.MembershipLevel membershipLevel) {
+		if (membershipLevel == null) {
+			return "GUEST";
+		}
+		return switch (membershipLevel) {
+			case FREE -> "FREE";
+			case PRO -> "PRO";
+		};
+	}
+
+	private void setGuestAttributes(Map<String, Object> attributes) {
+		attributes.put("userType", "GUEST");
+		attributes.put("chatCount", 0);
+		attributes.put("user", null);
+		attributes.put("userId", null);
 	}
 }
