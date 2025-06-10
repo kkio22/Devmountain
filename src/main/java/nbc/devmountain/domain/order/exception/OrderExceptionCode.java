b6246554251fbac3d2fca08879fcfbb057c2ab9d
hugecookie <phrpp5770@gmail.com>
2025-06-10 15:34:04 +0900

    feat: 예외 처리 오류 코드 추가
    ## 변경 이유
    - postgresql에서 User는 예약어 이므로 s를 추가함

--- src/main/java/nbc/devmountain/domain/order/exception/OrderExceptionCode.java
+++ src/main/java/nbc/devmountain/domain/order/exception/OrderExceptionCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public enum OrderExceptionCode implements ExceptionCode {
     ORDER_NOT_FOUND("404", "주문 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
-    USER_NOT_FOUND("404", "유저 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value());
+    USER_NOT_FOUND("404", "유저 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
+    NO_PERMISSION("403", "해당 주문에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN.value());
 
     private final String error;
     private final String message;
