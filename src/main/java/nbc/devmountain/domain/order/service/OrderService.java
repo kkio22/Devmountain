ea1dd426a63a7265bb36be03cafafaac216f5e8a
hugecookie <phrpp5770@gmail.com>
2025-06-10 15:35:01 +0900

    feat: 주문 처리 중에 자기 자신이거나 관리자만 조회 할 수 있도록 수정

--- src/main/java/nbc/devmountain/domain/order/service/OrderService.java
+++ src/main/java/nbc/devmountain/domain/order/service/OrderService.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import nbc.devmountain.domain.order.dto.OrderResponseDto;
 import nbc.devmountain.domain.order.dto.OrderStatusUpdateDto;
+import nbc.devmountain.domain.order.exception.OrderException;
+import nbc.devmountain.domain.order.exception.OrderExceptionCode;
 import nbc.devmountain.domain.order.model.Order;
 import nbc.devmountain.domain.order.model.OrderStatus;
 import nbc.devmountain.domain.order.repository.OrderRepository;
@@ -38,15 +40,28 @@ public class OrderService {
                 .toList();
     }
 
-    public OrderResponseDto getOrderById(Long orderId) {
-        return orderRepository.findById(orderId)
-                .map(OrderResponseDto::from)
-                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+    public OrderResponseDto getOrderById(Long orderId, Long currentUserId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.ORDER_NOT_FOUND));
+
+        User user = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.USER_NOT_FOUND));
+
+        if (!order.getId().equals(currentUserId) && user.getRole() != User.Role.ADMIN) {
+            throw new OrderException(OrderExceptionCode.NO_PERMISSION);
+        }
+
+        return OrderResponseDto.from(order);
     }
 
-    public void updateOrderStatus(Long orderId, OrderStatusUpdateDto dto) {
+    public void updateOrderStatus(Long orderId, OrderStatusUpdateDto dto, Long currentUserId) {
         Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.ORDER_NOT_FOUND));
+
+        if (!order.getId().equals(currentUserId)) {
+            throw new OrderException(OrderExceptionCode.NO_PERMISSION);
+        }
+
         order.updateOrderStatus(dto.status());
     }
 }
