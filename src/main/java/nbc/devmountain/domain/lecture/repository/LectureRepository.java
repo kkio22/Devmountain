8bdda2b050516f788af05f3ed59322a6161b9e07
류형철 <fbgudcjf2@gmail.com>
2025-06-10 18:07:28 +0900

    feat: 강의 키워드 조회 메서드 추가

--- src/main/java/nbc/devmountain/domain/lecture/repository/LectureRepository.java
+++ src/main/java/nbc/devmountain/domain/lecture/repository/LectureRepository.java
@@ -1,9 +1,15 @@
 package nbc.devmountain.domain.lecture.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import nbc.devmountain.domain.lecture.model.Lecture;
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+	@Query("SELECT l FROM Lecture l WHERE l.title LIKE %:keyword% OR l.description LIKE %:keyword%")
+	List<Lecture> findRelevantLectures(@Param("keyword") String keyword);
 }
