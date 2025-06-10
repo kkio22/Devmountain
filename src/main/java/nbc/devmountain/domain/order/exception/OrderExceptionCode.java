96bdf640ac19e250fc5c26b5b04ea501bf6d3563
hugecookie <phrpp5770@gmail.com>
2025-06-10 16:59:01 +0900

    feat: OrderExceptionCode 추가

--- src/main/java/nbc/devmountain/domain/order/exception/OrderExceptionCode.java
+++ src/main/java/nbc/devmountain/domain/order/exception/OrderExceptionCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum OrderExceptionCode implements ExceptionCode {
     ORDER_NOT_FOUND("404", "주문 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
     USER_NOT_FOUND("404", "유저 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND.value()),
-    NO_PERMISSION("403", "해당 주문에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN.value());
+    NO_PERMISSION("403", "해당 주문에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN.value()),
+    PAYMENT_FAILED("404", "결제를 실패했습니다.", HttpStatus.NOT_FOUND.value()),;
 
     private final String error;
     private final String message;
