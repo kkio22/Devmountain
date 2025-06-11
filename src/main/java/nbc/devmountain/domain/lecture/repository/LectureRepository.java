ae7a8f6274f8910216020bc040896605efc7a90a
kkio22 <nahyeon2252@gmail.com>
2025-06-11 20:19:56 +0900

    refactor: 강의 데이터가 삭제 예외처리

--- src/main/java/nbc/devmountain/domain/lecture/repository/LectureRepository.java
+++ src/main/java/nbc/devmountain/domain/lecture/repository/LectureRepository.java
@@ -1,9 +1,19 @@
 package nbc.devmountain.domain.lecture.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import nbc.devmountain.domain.lecture.model.Lecture;
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+	List<Lecture> findByLectureEmbeddingIsNull();
+
+	void deleteByCrawledAtBefore(@Param("today")LocalDateTime startDate);
 }
