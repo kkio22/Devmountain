c6780d34e8529c5fcfd7861af8e897391de79fdd
yunhyeok_lee <yuy9611@gmail.com>
2025-06-11 17:03:47 +0900

    feat : EmbeddingController 수정

--- src/main/java/nbc/devmountain/domain/ai/controller/EmbeddingController.java
+++ src/main/java/nbc/devmountain/domain/ai/controller/EmbeddingController.java
@@ -1,24 +1,23 @@
-// package nbc.devmountain.domain.ai.controller;
-//
-// import java.util.List;
-//
-// import lombok.RequiredArgsConstructor;
-//
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// import nbc.devmountain.domain.ai.service.EmbeddingService;
-//
-// @RestController
-// @RequiredArgsConstructor
-// public class EmbeddingController {
-//
-// 	private final EmbeddingService embeddingService;
-//
-// 	@GetMapping("/ai/embedding")
-// 	public List<Double> embed(@RequestParam String message) {
-// 		return embeddingService.getEmbedding(message);
-// 	}
-// }
-//
+package nbc.devmountain.domain.ai.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nbc.devmountain.domain.ai.service.EmbeddingService;
+
+@RestController
+@RequiredArgsConstructor
+public class EmbeddingController {
+
+	private final EmbeddingService embeddingService;
+
+	@GetMapping("/ai/embedding")
+	public List<Double> embed(@RequestParam String message) {
+		return embeddingService.getEmbedding(message);
+	}
+}
