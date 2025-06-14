2db17ddf26a2aa4b1d3890204b3a90e2d18eb853
hugecookie <phrpp5770@gmail.com>
2025-06-14 19:57:44 +0900

    feat: 필요 없는 컨트롤러 제거 후 서비스 로직 예외 처리 추가 작업 완료

--- src/main/java/nbc/devmountain/domain/order/repository/PaymentRepository.java
+++ src/main/java/nbc/devmountain/domain/order/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package nbc.devmountain.domain.order.repository;
 import nbc.devmountain.domain.order.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByPayIdAndUserUserId(Long payId, Long userId);
 }
--- src/main/java/nbc/devmountain/domain/order/service/PaymentService.java
+++ src/main/java/nbc/devmountain/domain/order/service/PaymentService.java
@@ -1,14 +1,13 @@
 package nbc.devmountain.domain.order.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import nbc.devmountain.domain.order.client.TossApiClient;
 import nbc.devmountain.domain.order.dto.ConfirmPaymentRequest;
-import nbc.devmountain.domain.order.dto.OrderPaymentRequestDto;
 import nbc.devmountain.domain.order.dto.PaymentResponseDto;
 import nbc.devmountain.domain.order.dto.TossPaymentResponse;
 import nbc.devmountain.domain.order.exception.OrderException;
 import nbc.devmountain.domain.order.exception.OrderExceptionCode;
+import nbc.devmountain.domain.order.model.Method;
 import nbc.devmountain.domain.order.model.Order;
 import nbc.devmountain.domain.order.model.OrderStatus;
 import nbc.devmountain.domain.order.model.Payment;
@@ -25,33 +24,6 @@ public class PaymentService {
     private final OrderRepository orderRepository;
     private final UserRepository userRepository;
     private final TossApiClient tossApiClient;
-
-    public PaymentResponseDto processPayment(Long orderId, Long userId, OrderPaymentRequestDto dto) {
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("User not found"));
-
-        // todo API 연결
-        Payment payment = Payment.builder()
-                .order(order)
-                .user(user)
-                .paymentKey(dto.paymentKey())
-                .method(dto.method())
-                .result(Payment.Result.SUCCESS)
-                .membershipLevelChangedTo(dto.membershipLevelChangedTo())
-                .build();
-
-        order.updateOrderStatus(OrderStatus.SUCCESS);
-        return PaymentResponseDto.from(paymentRepository.save(payment));
-    }
-
-    public PaymentResponseDto getPayment(Long paymentId) {
-        return paymentRepository.findById(paymentId)
-                .map(PaymentResponseDto::from)
-                .orElseThrow(() -> new EntityNotFoundException("Payment not found"));
-    }
-
 
     public TossPaymentResponse confirmPayment(ConfirmPaymentRequest request, Long userId) {
         Order order = orderRepository.findByOrderId(request.orderId())
@@ -95,13 +67,19 @@ public class PaymentService {
     }
 
     // 각 케이스별 ENUM 값 변환
-    private Payment.Method mapToMethodEnum(String method) {
+    private Method mapToMethodEnum(String method) {
         return switch (method) {
-            case "카드" -> Payment.Method.CARD;
-            case "가상계좌" -> Payment.Method.TRANSFER;
-            case "휴대폰" -> Payment.Method.PHONE;
-            case "간편결제" -> Payment.Method.SIMPLE_PAYMENT;
+            case "카드" -> Method.CARD;
+            case "가상계좌" -> Method.TRANSFER;
+            case "휴대폰" -> Method.PHONE;
+            case "간편결제" -> Method.SIMPLE_PAYMENT;
             default -> throw new OrderException(OrderExceptionCode.ORDER_NOT_FOUND);
         };
+    }
+
+    public PaymentResponseDto getPayment(Long paymentId, Long userId) {
+        return paymentRepository.findByPayIdAndUserUserId(paymentId, userId)
+                .map(PaymentResponseDto::from)
+                .orElseThrow(() -> new OrderException(OrderExceptionCode.NO_PERMISSION));
     }
 }
