10c1778b89e4a774d37abf5de0f8edaf7e5ac20d
kkio22 <nahyeon2252@gmail.com>
2025-06-26 18:46:02 +0900

    feat : 주석 삭제

--- src/main/java/nbc/devmountain/common/config/RedisConfig.java
+++ src/main/java/nbc/devmountain/common/config/RedisConfig.java
@@ -1,11 +1,9 @@
 package nbc.devmountain.common.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,11 +16,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class RedisConfig {
 
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
-		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-		redisStandaloneConfiguration.setPort(redisProperties.getPort());
-		redisStandaloneConfiguration.setHostName(redisProperties.getHost());
-		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory();
 	}
 
 
