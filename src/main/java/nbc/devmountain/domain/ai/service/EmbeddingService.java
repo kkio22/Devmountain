b78bca319680d6a9e80881ce7c560a8911d43976
류형철 <fbgudcjf2@gmail.com>
2025-06-26 21:54:29 +0900

    feat: 사용자 키워드,강의정보간 유사도점수 계산

--- src/main/java/nbc/devmountain/domain/ai/service/EmbeddingService.java
+++ src/main/java/nbc/devmountain/domain/ai/service/EmbeddingService.java
@@ -20,7 +20,7 @@ public class EmbeddingService {
 	 * getOutput() : 첫 번째 임베딩 결과에서 float 배열 형태의 벡터 추출 / 실제 텍스트 수치적 표현
 	 * @return 임베딩 벡터 (List<Double>)
 	 */
-	public List<Double> getEmbedding(String content) {
+	public float[] getEmbedding(String content) {
 		// 1. 텍스트를 리스트로 감싸서 요청
 		EmbeddingResponse response = embeddingModel.embedForResponse(List.of(content));
 
@@ -33,14 +33,7 @@ public class EmbeddingService {
 		if (embedding == null) {
 			throw new IllegalStateException("임베딩 결과 벡터가 null입니다.");
 		}
-
-		// float 배열을 Double 리스트로 변환
-		List<Double> listEmbedding = new ArrayList<>();
-		for (float value : embedding) {
-			listEmbedding.add((double)value);
-		}
-
-		return listEmbedding;
+		return embedding;
 	}
 }
 
