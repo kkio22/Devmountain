5117c4b6cd134a2072cb64b11f5b3453d609c7e6
kkio22 <nahyeon2252@gmail.com>
2025-06-26 15:52:32 +0900

    feat : 강의 임베딩 서비스 로직 코드를 spring batch 사용해서 리팩토링

--- src/main/java/nbc/devmountain/domain/lecture/batch/embedding/EmbeddingWriter.java
+++ src/main/java/nbc/devmountain/domain/lecture/batch/embedding/EmbeddingWriter.java
@@ -1,4 +1,33 @@
 package nbc.devmountain.domain.lecture.batch.embedding;
 
-public class EmbeddingWriter {
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@StepScope
+@RequiredArgsConstructor
+@Slf4j
+public class EmbeddingWriter implements ItemWriter<Document> {
+	private final VectorStore vectorStore;
+
+	@Override
+	public void write(Chunk<? extends Document> documents) throws Exception {
+
+	/*
+	chunk<Document> document 형태 즉 List<Documnet> document이고 document 500개가 한번에 들어온 것임
+	 */
+
+		log.info("들어온 강의 개수: {}", documents.size());
+
+		vectorStore.add((List<Document>)documents);
+
+		log.info("임베딩 완료");
+	}
 }
